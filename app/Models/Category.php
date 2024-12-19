0d672b5f5e6cddacb7af45d6cc750faeee1e3e2e
Charles Gendron <charlesgendron@Charless-MacBook-Air.local>
2024-12-19 10:08:24 -0300

    added recovery to header

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -45,12 +45,6 @@ class Category extends Model
     protected static function boot()
     {
         parent::boot();
-        
-        static::creating(function ($category) {
-            if (empty($category->slug)) {
-                $category->slug = Str::slug($category->name);
-            }
-        });
     }
 
     /**
